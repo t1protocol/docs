4a542690b27e0cacd0a33181d755257f988b57b0
Evan Chipman <42247026+evchip@users.noreply.github.com>
2025-07-16 11:26:00 +0100

    Update API docs for Read Result (#33)
    * update references of direction query param for read result API to Arbitrum and Base instead of L1 and L2 respectively
    
    * change references of v05 to v06
    
    ---------
    
    Signed-off-by: Evan Chipman <42247026+evchip@users.noreply.github.com>

--- examples/sdk/fetchAndVerifyProofOfRead.ts
+++ examples/sdk/fetchAndVerifyProofOfRead.ts
@@ -19,8 +19,8 @@
 import { ethers } from 'ethers';
 
 enum Direction {
-  ARB_TO_BASE = 'L1_TO_L2',
-  BASE_TO_ARB = 'L2_TO_L1',
+  ARB_TO_BASE = 'ARB_TO_BASE',
+  BASE_TO_ARB = 'BASE_TO_ARB',
 }
 
 interface ClaimInfo {
@@ -63,7 +63,7 @@ interface VerifiedProofReturn {
 const XCHAINREADER_ABI = [
   'function verifyProofOfRead(bytes encodedProofOfRead) external view returns (bytes32, bytes)',
 ];
-const API_BASE_URL = 'https://api.v05.t1protocol.com/api';
+const API_BASE_URL = 'https://api.v06.t1protocol.com/api';
 const ARB_RPC_URL = 'https://arbitrum-sepolia-rpc.publicnode.com';
 const BASE_RPC_URL = 'https://base-sepolia-rpc.publicnode.com';
 const ARB_XCHAINREADER_ADDRESS = '0x42d389A9007e446b92C0ce7bd8F42Ea10292881B';
